6d79ace73415341ae54cea28633aab0015c27937
Mohd Arish <arishmd768@gmail>
2023-10-14 00:11:12 +0530

    first commit

---
+++ controllers/project.js
@@ -0,0 +1,9 @@
+
+export const createProduct= async(req,res)=>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routes/product.js";
+
+const app=express();
+dotenv.config();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+app.use("/product",productRouter);
+
+const CONNECTION_URL =process.env.MONGO_URL;
+const PORT = process.env.PORT|| 5001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
